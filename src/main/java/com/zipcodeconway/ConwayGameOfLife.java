eb62868ae40a0e1cf8030cad391f42214eb7bdc7
Jared <jared@Zipcoders-MacBook-Pro-4.local>
2024-07-24 10:23:17 -0400

    complete

--- src/main/java/com/zipcodeconway/ConwayGameOfLife.java
+++ src/main/java/com/zipcodeconway/ConwayGameOfLife.java
@@ -1,43 +1,80 @@
 package com.zipcodeconway;
 
+import java.util.Random;
+
 public class ConwayGameOfLife {
+    public SimpleWindow displayWindow;
+    public int dimension;
+    public int[][] currentState;
 
     public ConwayGameOfLife(Integer dimension) {
+        this.displayWindow = new SimpleWindow(dimension);
+        this.dimension = dimension;
+        currentState = createRandomStart(dimension);
      }
 
-    public ConwayGameOfLife(Integer dimension, int[][] startmatrix) {
+    public ConwayGameOfLife(Integer dimension, int[][] startMatrix) {
+        this.displayWindow = new SimpleWindow(dimension);
+        this.dimension = dimension;
+        this.currentState = startMatrix;
     }
 
     public static void main(String[] args) {
         ConwayGameOfLife sim = new ConwayGameOfLife(50);
-        int[][] endingWorld = sim.simulate(50);
+        sim.createRandomStart(50);
+        sim.simulate(100);
     }
 
-    // Contains the logic for the starting scenario.
-    // Which cells are alive or dead in generation 0.
-    // allocates and returns the starting matrix of size 'dimension'
     private int[][] createRandomStart(Integer dimension) {
-        return new int[1][1];
+        Random random = new Random(System.currentTimeMillis());
+        int[][] randomSeed = new int[dimension][dimension];
+        for (int i = 0; i < randomSeed.length; i++){
+            for (int j = 0; j < randomSeed.length; j++){
+                int randomNum = random.nextInt(2);
+                randomSeed[i][j] = randomNum;
+            }
+        }
+        return randomSeed;
     }
 
     public int[][] simulate(Integer maxGenerations) {
-        return new int[1][1];
+        int[][] nextGen = new int[dimension][dimension];
+        for(int i = 0; i <= maxGenerations; i++){
+            for(int j = 0; j < dimension; j++){
+                for(int k = 0; k < dimension; k++){
+
+                    int updatedStatus = isAlive(j, k, currentState);
+                    nextGen[j][k] = updatedStatus;
+                }
+            }
+            copyAndZeroOut(nextGen, currentState);
+            this.displayWindow.display(currentState, i);
+            this.displayWindow.sleep(100);
+        }
+        return nextGen;
     }
 
-    // copy the values of 'next' matrix to 'current' matrix,
-    // and then zero out the contents of 'next' matrix
     public void copyAndZeroOut(int [][] next, int[][] current) {
+        for (int i = 0; i < current.length; i++) {
+            System.arraycopy(next[i], 0, current[i], 0, current[i].length);
+        }
     }
 
-    // Calculate if an individual cell should be alive in the next generation.
-    // Based on the game logic:
-	/*
-		Any live cell with fewer than two live neighbours dies, as if by needs caused by underpopulation.
-		Any live cell with more than three live neighbours dies, as if by overcrowding.
-		Any live cell with two or three live neighbours lives, unchanged, to the next generation.
-		Any dead cell with exactly three live neighbours cells will come to life.
-	*/
     private int isAlive(int row, int col, int[][] world) {
-        return 0;
+        int sum = 0;
+        int[][] coordinates = new int[][] {{ row - 1, col }, { row, col + 1}, { row + 1, col }, { row, col - 1},
+                { row - 1, col + 1}, { row + 1, col + 1 }, { row + 1, col - 1}, { row - 1, col - 1}};
+
+        for(int[] eachCoordinate : coordinates){
+            try {
+                sum += world[eachCoordinate[0]][eachCoordinate[1]];
+            } catch (Exception ignored) {} //ignores out of bounds exception
+        }
+
+        boolean isCellAlive = world[row][col] == 1;
+
+//         if cell is alive, check sum < 2 OR > 3 then turn dead (0), otherwise cell is alive (1)
+//         if cell is dead, check if neighbors == 3, revive (1)
+        return isCellAlive ? ((sum < 2 || sum > 3) ? 0 : 1) : (sum == 3 ? 1 : 0);
     }
 }
